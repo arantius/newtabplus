7ffecc04e2d0eef920d2fa83419547d6277e7add
Anthony Lieuallen <arantius@gmail.com>
2013-06-28 20:31:56 -0400

    Initial checkin.

---
+++ content/newtabplus-overlay.js
@@ -0,0 +1,26 @@
+(function() {
+  Components.utils.import("resource://gre/modules/PrivateBrowsingUtils.jsm");
+  let ss = Components.classes["@mozilla.org/browser/sessionstore;1"]
+      .getService(Components.interfaces.nsISessionStore);
+
+  let chromeWin = window
+      .QueryInterface(Components.interfaces.nsIInterfaceRequestor)
+      .getInterface(Components.interfaces.nsIWebNavigation)
+      .QueryInterface(Components.interfaces.nsIDocShellTreeItem)
+      .rootTreeItem
+      .QueryInterface(Components.interfaces.nsIInterfaceRequestor)
+      .getInterface(Components.interfaces.nsIDOMWindow);
+
+  if (PrivateBrowsingUtils.isWindowPrivate(window)
+      || ss.getClosedTabCount(chromeWin) == 0
+      || true
+  ) {
+    document.getElementById('newtab-recently-closed').display = 'none';
+    return;
+  }
+
+  let undoItems = JSON.parse(ss.getClosedTabData(chromeWin));
+  for (let i = 0; i < undoItems.length; i++) {
+  }
+
+})();
