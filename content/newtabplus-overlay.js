3c6a08ee48fd9422cfca69925e48b92808782933
Anthony Lieuallen <arantius@gmail.com>
2013-06-28 21:24:07 -0400

    basic navigation functions

--- content/newtabplus-overlay.js
+++ content/newtabplus-overlay.js
@@ -18,7 +18,30 @@ window.addEventListener('DOMContentLoaded', (function() {
     return;
   }
 
+  function nav(event) {
+    chromeWin.undoCloseTab(parseInt(event.target.getAttribute('index'), 10));
+    window.close();
+  }
+
+  let cont = document.getElementById('newtab-recently-closed-list');
   let undoItems = JSON.parse(ss.getClosedTabData(chromeWin));
   for (let i = 0; i < undoItems.length; i++) {
+    let undoItem = undoItems[i];
+    let li = document.createElement('li');
+    li.textContent = undoItem.title;
+    li.setAttribute('index', i);
+    li.addEventListener('click', nav, false);
+
+    if (undoItem.image) {
+      let img = document.createElement('img');
+      let iconUrl = undoItem.image;
+      if (/^https?:/.test(iconUrl)) iconUrl = 'moz-anno:favicon:' + iconUrl;
+      img.setAttribute('src', iconUrl);
+      img.setAttribute('width', '16');
+      img.setAttribute('height', '16');
+      li.appendChild(img);
+    }
+
+    cont.appendChild(li);
   }
 }), false);
